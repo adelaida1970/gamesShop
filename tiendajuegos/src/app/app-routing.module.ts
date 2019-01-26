1c66e03da5bb695e7f1bcf379021041a730848d7
alexis duque <alexis.duque1993@gmail.com>
2019-01-26 18:52:27 -0500

    changes

--- tiendajuegos/src/app/app-routing.module.ts
+++ tiendajuegos/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { TestComponent } from './test/test.component';
+import { NavComponent } from './nav/nav.component';
 const routes: Routes = [
   { path: '', redirectTo: 'header', pathMatch: 'full' },     
   { path: 'header', component: HeaderComponent  },
-  { path: 'test', component: TestComponent },
+  { path: 'nav', component: NavComponent },
   
 ];
 
